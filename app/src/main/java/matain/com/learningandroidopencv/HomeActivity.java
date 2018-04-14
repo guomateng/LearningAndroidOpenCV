b7ae5cd816e91a84accbad712b297d216d8bdcab
guomateng <875962735@qq.com>
2018-04-14 10:22:19 +0800

    Learning OpenCV for Android project init ....

---
+++ app/src/main/java/matain/com/learningandroidopencv/HomeActivity.java
@@ -0,0 +1,13 @@
+package matain.com.learningandroidopencv;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
